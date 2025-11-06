fcdfc69e9801e1f9ba7ca6e48d34f219436de9dd
HohnMarton <marcika0825@gmail.com>
2025-11-06 09:26:21 +0100

    css kisebb javitások

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
-const express = require('express'); const path = require('path'); const app = express(); const PORT = process.env.PORT || 3000;
+const express = require('express'); 
+const path = require('path'); 
+const app = express(); 
+const PORT = process.env.PORT || 3000;
 app.use(express.static(__dirname));
+
 app.get('/', (req,res)=>res.sendFile(path.join(__dirname,'index.html')));
+
 app.get('/login', (req,res)=>res.sendFile(path.join(__dirname,'login.html')));
+
 app.use((req,res)=>res.status(404).sendFile(path.join(__dirname,'index.html')));
 app.listen(PORT, ()=>console.log(`✅ Server running: http://localhost:${PORT}`));
